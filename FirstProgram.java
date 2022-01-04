5da2bd9dec1f5d2b8d99da87500e860a65498815
Kiran2612 <kiranvaland@hotmail.com>
2022-01-04 21:20:29 +0000

    this is my first commit

---
+++ FirstProgram.java
@@ -0,0 +1,12 @@
+package com.myfirstprogram;
+
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        System.out.print("Kiran");
+        System.out.println(" Valand");
+
+        System.out.print("Kiran");
+        System.out.println(" Valand");
+    }
+}
